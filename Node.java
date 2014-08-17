e76e86a8251eca10ba6a4074a0b95fac1140c0de
Jeff Senecal <jeffsenecal79@gmail.com>
2014-08-17 15:56:56 -0700

    update

--- Node.java
+++ Node.java
@@ -1,141 +1,116 @@
-///////////////////////////////////////////////////////////////////////
-// Node.java - Class representing nodes in the search tree
-// Jeff Senecal
-// CS 482 Exercise #2
-
-import java.util.*;
-
 public class Node implements Comparable<Node> {
-	
-	private State s;	// The state for this node
-	private Node back;	// Back pointer
-	private byte op;	// Operator used to get to this node
-	private short g;	// Depth of node in search tree
-	private short h;	// Computed heuristic for node state
-	
-	///////////////////////////////////////////////////////////////////
-	// Constructor
-	public Node(State s, Node back, byte op, short h)
-	{
-		// Initialize member variables
-		this.s = s;
-		this.back = back;
-		this.op = op;
-		this.h = h;
-		
-		// Set root node depth to 0, and child node depth to
-		// parent depth + 1
-		if (back == null)
-			this.g = 0;
-		else
-			this.g = (short)(back.g + 1);
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Setter for back pointer
-	public void setBackPtr(Node back)
-	{
-		this.back = back;
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Setter for operator
-	public void setOp(byte op)
-	{
-		this.op = op;
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Computes and returns f = depth + heuristic
-	public short getF()
-	{
-		return (short)(g+h);
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Setter for tree depth
-	public void setG(short g)
-	{
-		this.g = g;
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Getter for tree depth
-	public short getG()
-	{
-		return g;
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Getter for state
-	public State getState()
-	{
-		return s;
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Returns a string representing the path from initial state to
-	// the state of this node
-	public String pathToString()
-	{
-		if (back != null)
-			return back.pathToString() + "\n" + opString(op) + "\n" + s;
-		else
-			return "\nInitial State:\n" + s;
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Returns a string representing the path from the current node to
-	// the goal node, skipping the first node in the path to prevent
-	// printing the middle node twice
-	public String revPathToStringSkipFirst()
-	{
-		if (back != null)
-			return "\n" + opString((byte)-op) + "\n" + back.revPathToString();
-		else
-			return "\n" + opString((byte)-op) + "\n";
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Returns a string representing the path from the current node to
-	// the goal node
-	public String revPathToString()
-	{
-		if (back != null)
-			return s + "\n" + opString((byte)-op) + "\n" + back.revPathToString();
-		else
-			return s.toString();
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// This function is used by the heap to provide a natural ordering
-	// for the Node objects
-	public int compareTo(Node otherNode)
-	{
-		if (this.getF() < otherNode.getF())
-			return -1;
-		else if (this.getF() == otherNode.getF())
-			return 0;
-		else
-			return 1;
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// Returns a string representing the type of operator used
-	private String opString(byte op)
-	{
-		switch(op)
-		{
-			case State.Left:
-				return "Left";
-			case State.Right:
-				return "Right";
-			case State.Down:
-				return "Down";
-			case State.Up:
-				return "Up";
-			default:
-				return "";
-		}
-	}
+    
+    private State state;            // The state for this node
+    private Node backPtr;            // Back pointer
+    private State.Operator op;        // Operator used to get to this node
+    private short g;                // Depth of node in search tree
+    private short h;                // Computed heuristic for node state
+    
+    /**
+     * @param state    Puzzle state associated with this node
+     * @param backPtr  Back pointer
+     * @param op       Operation that led to this node
+     * @param h        Heuristic for node state
+     */
+    public Node(State state, Node backPtr, State.Operator op, short h) {
+        this.state = state;
+        this.backPtr = backPtr;
+        this.op = op;
+        this.h = h;
+        
+        // Set root node depth to 0, and child node depth to
+        // parent depth + 1
+        if (backPtr == null)
+            this.g = 0;
+        else
+            this.g = (short)(backPtr.g + 1);
+    }
+    
+    /**
+     * @param backPtr  Back pointer to previous node
+     */
+    public void setBackPtr(Node backPtr) {
+        this.backPtr = backPtr;
+    }
+
+    /**
+     * @param op  Operator that led to this state
+     */
+    public void setOp(State.Operator op) {
+        this.op = op;
+    }
+    
+    /**
+     * @return  Tree depth plus heuristic
+     */
+    public short getF() {
+        return (short)(g+h);
+    }
+    
+    /**
+     * @param g  Tree depth
+     */
+    public void setG(short g) {
+        this.g = g;
+    }
+    
+    /**
+     * @return  Tree depth
+     */
+    public short getG() {
+        return g;
+    }
+    
+    /**
+     * @return  Game state of the node
+     */
+    public State getState() {
+        return state;
+    }
+    
+    /**
+     * @return  String representing the path from initial state to
+     *          the state of this node.
+     */
+    public String pathToString() {
+        if (backPtr != null)
+            return backPtr.pathToString() + "\n" + op + "\n" + state;
+        else
+            return "\nInitial State:\n" + state;
+    }
+    
+    /**
+     * @return  String representing the path from the current node to
+     *          the goal node, skipping the first node in the path to prevent
+     *          printing the middle node twice
+     */
+    public String revPathToStringSkipFirst() {
+        if (backPtr != null)
+            return "\n" + op.reverse() + "\n" + backPtr.revPathToString();
+        else
+            return "\n" + op.reverse() + "\n";
+    }
+    
+    /**
+     * @return  String representing the path from the current node to the goal node.
+     */
+    public String revPathToString() {
+        if (backPtr != null)
+            return state + "\n" + op.reverse() + "\n" + backPtr.revPathToString();
+        else
+            return state.toString();
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Comparable#compareTo(java.lang.Object)
+     */
+    @Override
+    public int compareTo(Node otherNode) {
+        if (this.getF() < otherNode.getF())
+            return -1;
+        else if (this.getF() == otherNode.getF())
+            return 0;
+        else
+            return 1;
+    }
 }
--- Puzzle.java
+++ Puzzle.java
@@ -1,187 +1,207 @@
-///////////////////////////////////////////////////////////////////////
-// Puzzle.java - main class for 15-puzzle solver
-// Jeff Senecal
-// CS 482 Exercise #2
-
-import java.util.*;
-import java.lang.reflect.Array;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Puzzle {
-	
-	static class NodeHeap extends PriorityQueue<Node> {}
-	static class StateNodeHash extends HashMap<State, Node> {}
-	
-	static final double HWEIGHT = 1.3;
-	
-	///////////////////////////////////////////////////////////////////
-	// main - run solver on test cases and print the results
-	public static void main(String args[])
-	{
-		State[] initials = new State[3];
-		
-		// Test cases
-		initials[0] = new State(new byte[][] {{2,10,3,0},{1,6,8,4},{5,9,7,12},{13,14,11,15}});
-		initials[1] = new State(new byte[][] {{2,11,3,10},{1,9,4,0},{6,13,7,8},{5,14,15,12}});
-		initials[2] = new State(new byte[][] {{4,3,2,1},{8,7,6,5},{12,11,10,9},{0,14,15,13}});
-		
-		// Goal state
-		State goal = new State(new byte[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}});
+    
+    // Weighting of heuristic in A* search
+    static final double HWEIGHT = 1.3;
+    
+    /**
+     * 15 Puzzle Solver
+     * 
+     * @param args File to read initial puzzle states from
+     */
+    public static void main(String args[]) {
+        
+        // Initial states of puzzles to solve
+        List<State> initials = new ArrayList<>();
+        
+        // Read input from file given on command line
+        Scanner s = null;
+        try {
+            s = new Scanner(new FileInputStream(args[0]));
+        } catch (FileNotFoundException e) {
+            System.out.println("Couldn't open input file '" + args[0] + "'");
+            System.exit(1);
+        }
 
-		// Run solver on each test case
-		for (State initial : initials)
-		{
-			Node[] solution = biDirectionalSolve(initial, goal);
-			if (solution != null)
-			{
-				// Output path
-				System.out.println("Path Length: " + (solution[0].getG() + solution[1].getG()));
-				System.out.print(solution[0].pathToString());
-				System.out.print(solution[1].revPathToStringSkipFirst() + "\n\n");
-			}
-		}
-	}
-	
-	///////////////////////////////////////////////////////////////////
-	// biDirectionalSolve -	solve 15-puzzle using bidirectional A*
-	//						search
-	// Arguments:	fwdInitial - initial state
-	// 				fwdGoal - goal state
-	// Returns:		Array of 2 Nodes where both forward and backward
-	//				paths meet.  First node has back pointer towards
-	//				initial state and second node has back pointer
-	//				towards goal state.
-	public static Node[] biDirectionalSolve(State fwdInitial, State fwdGoal)
-	{
-		final int FWD = 0;		// Forward direction
-		final int REV = 1;		// Backward direction
-		
-		byte[] ops = {State.Left, State.Right, State.Up, State.Down};
-		int[] directions = {FWD, REV};
-		
-		// These arrays of objects have two elements, one for the forward
-		// direction and one for the backward direction.
-		
-		// Min-heap for removing the node from the open set with the
-		// smallest f-score.
-		NodeHeap[] openHeap = new NodeHeap[2];
-		
-		// Hash tables with States as keys and Nodes as data for
-		// checking if a state is in the open or closed set.
-		StateNodeHash[] openHash = new StateNodeHash[2];
-		StateNodeHash[] closedHash = new StateNodeHash[2];
-		
-		// Initial and goal states
-		State[] initial = new State[] {fwdInitial, fwdGoal};
-		State[] goal = new State[] {fwdGoal, fwdInitial};
-		
-		// For both forward and backward directions
-		for (int i : directions)
-		{
-			// Create empty heap and hash tables
-			openHeap[i] = new NodeHeap();
-			openHash[i] = new StateNodeHash();
-			closedHash[i] = new StateNodeHash();
-			
-			// Add initial node to the open set
-			Node n = new Node(initial[i], null, State.None, (short)(initial[i].h(goal[i]) * HWEIGHT));
-			openHash[i].put(initial[i], n);
-			openHeap[i].add(n);
-		}
-		
-		// For first iteration we start from the forward direction
-		int i = FWD; // Index into our arrays for the current direction
-		int j = REV; // Index into our arrays for the opposite direction
-		
-		// While there are still elements in the open set
-		while(!openHeap[i].isEmpty())
-		{
-			// Remove node with minimum f-score
-			Node n = openHeap[i].poll();
-			State s = n.getState();
-				
-			// Move the node from the open to closed set
-			openHash[i].remove(s);
-			closedHash[i].put(s, n);
-			
-			// For each of the four possible operators
-			for (byte op : ops)
-			{
-				// Create a new state that is the result of the move
-				State newState = s.move(op);
-				
-				// If the move is invalid or has already been tried,
-				// go on to next move
-				if (newState == null || closedHash[i].containsKey(newState))
-					continue;
-					
-				// If the new state is not already in the open set
-				if (!openHash[i].containsKey(newState))
-				{
-					// Create a new Node for this state
-					Node newNode = new Node(newState, n, op, (short)(newState.h(goal[i]) * HWEIGHT));
-					
-					// Check for a match in the nodes of the opposite direction
-					Node matchedNode = null;
-					if (openHash[j].containsKey(newState))
-						matchedNode = openHash[j].get(newState);
-					if (closedHash[j].containsKey(newState))
-						matchedNode = closedHash[j].get(newState);
-					
-					// If there is a match, return the pair of nodes
-					if (matchedNode != null)
-					{
-						int openNodeCount = openHash[i].size() + openHash[j].size() + 1;
-						int closedNodeCount = closedHash[i].size() + closedHash[i].size();
-						
-						System.out.print("Nodes Generated: " + (openNodeCount + closedNodeCount));
-						System.out.print(" (" + openNodeCount + " open/");
-						System.out.println(closedNodeCount + " closed)");
-						
-						if (i == FWD)
-							return new Node[]{newNode, matchedNode};
-						else
-							return new Node[]{matchedNode, newNode};
-					// Otherwise, add the new node to the open set
-					} else {
-						openHash[i].put(newState, newNode);
-						openHeap[i].add(newNode);
-					}
-				}
-				// If the new state is already in the open set
-				else
-				{
-					// Retrieve the existing node
-					Node existingNode = openHash[i].get(newState);
-					
-					// If we have found a shorter path to this node
-					if (n.getG() + 1 < existingNode.getG()) {
-						
-						// Update node depth, back pointer, and operator
-						existingNode.setG((short)(n.getG() + 1));
-						existingNode.setBackPtr(n);
-						existingNode.setOp(op);
-						
-						// Remove and re-add node from heap.  This will
-						// cause the node to be placed in the proper
-						// minheap order.
-						openHeap[i].remove(existingNode);
-						openHeap[i].add(existingNode);
-					}
-				}
-			}
-			
-			// Swap directions
-			if (i == FWD)
-			{
-				i = REV;
-				j = FWD;
-			}
-			else
-			{
-				i = FWD;
-				j = REV;
-			}
-		}
-		return null;	// No solution found
-	}
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            
+            // Skip blank lines
+            if (line.isEmpty())
+                continue;
+            
+            Scanner ss = new Scanner(line);
+            byte[][] board = new byte[4][4];
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if (ss.hasNextInt()) {
+                        board[i][j] = (byte) ss.nextInt();
+                    } else {
+                        System.out.println("Invalid input file");
+                        System.exit(1);
+                    }
+                }
+            }
+            ss.close();
+            
+            initials.add(new State(board));
+        }
+        s.close();
+        
+        // Goal state
+        State goal = new State(new byte[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}});
+
+        // Run solver on each test case
+        for (State initial : initials) {
+            Node[] solution = biDirectionalSolve(initial, goal);
+            if (solution != null) {
+                // Output path
+                System.out.println("Path Length: " + (solution[0].getG() + solution[1].getG()));
+                System.out.print(solution[0].pathToString());
+                System.out.print(solution[1].revPathToStringSkipFirst() + "\n\n");
+            }
+        }
+    }
+    
+    /**
+     * Solve 15-puzzle using bidirectional A* search
+     * 
+     * @param fwdInitial initial state
+     * @param fwdGoal    goal state
+     * @return Array of 2 Nodes where both forward and backward
+     *         paths meet.  First node has back pointer towards
+     *         initial state and second node has back pointer
+     *         towards goal state.
+     */
+    public static Node[] biDirectionalSolve(State fwdInitial, State fwdGoal) {
+        
+        final int FWD = 0;        // Forward direction
+        final int REV = 1;        // Backward direction
+        
+        int[] directions = {FWD, REV};
+        
+        // These lists have two elements, one for the forward
+        // direction and one for the backward direction.
+        
+        // Min-heap for removing the node from the open set with the
+        // smallest f-score.
+        List<Queue<Node>> openHeap = new ArrayList<>(2);
+        
+        // Hash tables with States as keys and Nodes as data for
+        // checking if a state is in the open or closed set.
+        List<Map<State, Node>> openHash = new ArrayList<>(2);
+        List<Map<State, Node>> closedHash = new ArrayList<>(2);
+        
+        // Initial and goal states
+        State[] initial = new State[] {fwdInitial, fwdGoal};
+        State[] goal = new State[] {fwdGoal, fwdInitial};
+        
+        // For both forward and backward directions
+        for (int i : directions) {
+            // Create empty heap and hash maps
+            openHeap.add(new PriorityQueue<Node>());
+            openHash.add(new HashMap<State, Node>());
+            closedHash.add(new HashMap<State, Node>());
+            
+            // Add initial node to the open set
+            Node n = new Node(initial[i], null, State.Operator.None, (short)(initial[i].h(goal[i]) * HWEIGHT));
+            openHash.get(i).put(initial[i], n);
+            openHeap.get(i).add(n);
+        }
+        
+        // For first iteration we start from the forward direction
+        int i = FWD; // Index into our lists for the current direction
+        int j = REV; // Index into our lists for the opposite direction
+        
+        // While there are still elements in the open set
+        while(!openHeap.get(i).isEmpty()) {
+            // Remove node with minimum f-score
+            Node n = openHeap.get(i).poll();
+            State s = n.getState();
+                
+            // Move the node from the open to closed set
+            openHash.get(i).remove(s);
+            closedHash.get(i).put(s, n);
+            
+            // For each of the four possible operators
+            for (State.Operator op : State.Operator.mutatorValues()) {
+                // Create a new state that is the result of the move
+                State newState = s.move(op);
+                
+                // If the move is invalid or has already been tried,
+                // go on to next move
+                if (newState == null || closedHash.get(i).containsKey(newState))
+                    continue;
+                    
+                // If the new state is not already in the open set
+                if (!openHash.get(i).containsKey(newState)) {
+                    // Create a new Node for this state
+                    Node newNode = new Node(newState, n, op, (short)(newState.h(goal[i]) * HWEIGHT));
+                    
+                    // Check for a match in the nodes of the opposite direction
+                    Node matchedNode = null;
+                    if (openHash.get(j).containsKey(newState))
+                        matchedNode = openHash.get(j).get(newState);
+                    if (closedHash.get(j).containsKey(newState))
+                        matchedNode = closedHash.get(j).get(newState);
+                    
+                    // If there is a match, return the pair of nodes
+                    if (matchedNode != null) {
+                        int openNodeCount = openHash.get(i).size() + openHash.get(j).size() + 1;
+                        int closedNodeCount = closedHash.get(i).size() + closedHash.get(j).size();
+                        
+                        System.out.print("Nodes Generated: " + (openNodeCount + closedNodeCount));
+                        System.out.print(" (" + openNodeCount + " open/");
+                        System.out.println(closedNodeCount + " closed)");
+                        
+                        if (i == FWD)
+                            return new Node[]{newNode, matchedNode};
+                        else
+                            return new Node[]{matchedNode, newNode};
+                    // Otherwise, add the new node to the open set
+                    } else {
+                        openHash.get(i).put(newState, newNode);
+                        openHeap.get(i).add(newNode);
+                    }
+                } else { // If the new state is already in the open set
+                    // Retrieve the existing node
+                    Node existingNode = openHash.get(i).get(newState);
+                    
+                    // If we have found a shorter path to this node
+                    if (n.getG() + 1 < existingNode.getG()) {
+                        
+                        // Update node depth, back pointer, and operator
+                        existingNode.setG((short)(n.getG() + 1));
+                        existingNode.setBackPtr(n);
+                        existingNode.setOp(op);
+                        
+                        // Remove and re-add node from heap.  This will
+                        // cause the node to be placed in the proper
+                        // minheap order.
+                        openHeap.get(i).remove(existingNode);
+                        openHeap.get(i).add(existingNode);
+                    }
+                }
+            }
+            
+            // Swap directions
+            if (i == FWD) {
+                i = REV;
+                j = FWD;
+            } else {
+                i = FWD;
+                j = REV;
+            }
+        }
+        return null;    // No solution found
+    }
 }
